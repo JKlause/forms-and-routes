401548189e52348c51b4b22ac851d809de72399e
Joe Klause <joeklause@gmail.com>
2019-11-04 21:21:33 -0800

    added container tests

---
+++ src/containers/HomePage.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import HomePage from './HomePage';
+
+
+
+describe('HomePage container', () => {
+
+  it('renders HomePage component', () => {
+
+    // eslint-disable-next-line no-undef
+    global.fetch = jest.fn(() => Promise.resolve());
+    const wrapper = shallow(<HomePage history={{}}/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
