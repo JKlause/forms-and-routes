99961a70bb290064dfe217b24c8a31d24b253c7f
Joe Klause <joeklause@gmail.com>
2019-11-04 20:35:57 -0800

    added nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {
+  Link
+} from 'react-router-dom';
 import styles from './Header.css';
 
 const Header = () => (
-
-  <h1 className={styles.Header}>The Last Airbender Character</h1>
-
+  <header className={styles.Header}>
+    <h1>The Last Airbender Character</h1>
+    <nav>
+      <Link to="/">Home Page</Link>
+      <Link to="/listpage">Character List</Link>
+    </nav>
+  </header>
 );
 
 
